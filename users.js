07482c394b789bf9a635d7c865de4bc0f729519c
larraunpe <larraunpe@gmail.com>
2026-01-02 08:46:15 +0100

    Update users.js

--- users.js
+++ users.js
@@ -1,5 +1,22 @@
-// users.js - Usuarios Larraun Pilota Elkartea
-const USERS = {
-  "P26738": { year: "2014", name: "DIEGO", phone: "669866803", email: "pelliki@hotmail.com", role: "user" },
-  "LARRAUNPE": { year: "200@", name: "Administrador", phone: "669866803", email: "larraunpe@gmail.com", role: "admin" }
+/* users.js */
+/* ATENCIÓN: ESTE ARCHIVO ES PÚBLICO SI SE SIRVE DIRECTAMENTE */
+
+const users = {
+  "LARRAUNPE": {
+    year: "200@",
+    role: "admin",
+    name: "Administrador",
+    phone: "669866803",
+    email: "larraunpe@gmail.com"
+  },
+
+  "P26738": {
+    year: "2014",
+    role: "user",
+    name: "DIEGO",
+    phone: "669866803",
+    email: "pelliki@hotmail.com"
+  }
+
+  /* AQUÍ SEGUIRÍAN EL RESTO DE USUARIOS */
 };
