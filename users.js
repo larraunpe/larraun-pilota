2377b2b32d020120ee7e9085eae82ff8d407c208
larraunpe <larraunpe@gmail.com>
2026-01-02 08:24:16 +0100

    Add files via upload

---
+++ users.js
@@ -0,0 +1,5 @@
+// users.js - Usuarios Larraun Pilota Elkartea
+const USERS = {
+  "P26738": { year: "2014", name: "DIEGO", phone: "669866803", email: "pelliki@hotmail.com", role: "user" },
+  "LARRAUNPE": { year: "200@", name: "Administrador", phone: "669866803", email: "larraunpe@gmail.com", role: "admin" }
+};
