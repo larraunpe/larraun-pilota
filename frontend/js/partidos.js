7deb5465c0caeea077abe47e37b68d0f525c1853
larraunpe <larraunpe@gmail.com>
2026-02-01 18:41:28 +0100

    Create partidos.js

---
+++ frontend/js/partidos.js
@@ -0,0 +1,25 @@
+const API = "https://TU-BACKEND.onrender.com";
+
+document.getElementById("partidoForm").addEventListener("submit", async e => {
+  e.preventDefault();
+
+  const partido = {
+    fecha: document.getElementById("fecha").value.replaceAll("-", "/"),
+    hora: document.getElementById("hora").value,
+    zkia: document.getElementById("zkia").value,
+    fronton: document.getElementById("fronton").value,
+    etxekoa: document.getElementById("etxekoa").value,
+    kanpokoak: document.getElementById("kanpokoak").value,
+    lehiaketa: document.getElementById("lehiaketa").value,
+    ofiziala: document.getElementById("ofiziala").value
+  };
+
+  await fetch(`${API}/partidos`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(partido)
+  });
+
+  alert("Partido guardado ✔️");
+  e.target.reset();
+});
